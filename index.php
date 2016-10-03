7b62161842ba887da29c237e28bfa0f7dc67e1db
Jerome Grosdenier <jerome@MacBook-Pro-de-Jerome.local>
2016-10-03 17:01:47 +0200

    Listing pages in app state and building the menu

--- index.php
+++ index.php
@@ -5,9 +5,13 @@
         <title>Hello React!</title>
         <meta charset="<?php bloginfo( 'charset' ); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <script src="<?php echo get_stylesheet_directory_uri(); ?>/build/react.min.js"></script>
         <script src="<?php echo get_stylesheet_directory_uri(); ?>/build/react-dom.min.js"></script>
         <script src="https://unpkg.com/babel-core@5.8.38/browser.min.js"></script>
+        <script>
+            var API_URI = "<?php echo home_url()?>/wp-json/wp/v2";
+        </script>
     </head>
     <body>
         <div id="content"></div>
--- src/wp-react-theme.js
+++ src/wp-react-theme.js
@@ -1,4 +1,104 @@
+var App = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<Header menuItems={this.state.pages}/>
+				<Content />
+				<Footer />
+			</div>
+		);
+	},
+
+	componentDidMount: function() {
+
+		// Getting Pages
+		var pages = [];
+		$.ajax({
+            type: "GET",
+            url: API_URI+"/pages",
+            success: function(response){
+
+            	for (var i = response.length - 1; i >= 0; i--) {
+            		var page = response[i];
+
+            		pages.push({
+            			slug: page.slug,
+            			title: page.title.rendered,
+            			content: page.content.rendered,
+            			uri: page.link,
+            		});
+            	}
+
+		        this.setState({
+		            pages: pages
+		        })
+            }.bind(this)
+        });
+	},
+
+	getInitialState: function(){
+		return {
+			pages : []
+		}
+	},
+});
+
+var Content = React.createClass({
+	render: function() {
+		return (
+			<main>Main</main>
+		);
+	}
+});
+
+var Menu = React.createClass({
+
+	render: function() {
+		var menuItems = this.props.menuItems.map(function(item) {
+            return <MenuItem key={item.slug} item={item} />
+        });
+
+		return (
+			<nav>
+				{menuItems}
+			</nav>
+		);
+	}
+});
+
+var MenuItem = React.createClass({
+    
+    render: function(){
+        return (
+        	<li>
+        		<a href={this.props.item.uri}>
+        			{this.props.item.title}
+        		</a>
+        	</li>
+        );
+    }
+});
+
+var Header = React.createClass({
+	render: function() {
+		return (
+			<header>
+				Header
+				<Menu menuItems={this.props.menuItems}/>
+			</header>
+		);
+	}
+});
+
+var Footer = React.createClass({
+	render: function() {
+		return (
+			<footer>Footer</footer>
+		);
+	}
+});
+
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    <App />,
     document.getElementById('content')
 );
